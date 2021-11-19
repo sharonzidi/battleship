0f59d01aab0fca0063ef061ffa62411ef9812cce
Zidi Xia <xia.zid@northeastern.edu>
2021-11-18 23:13:15 -0800

    finished

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,11 +4,13 @@ import './Css/Navbar.css';
 
 export default function Navbar() {
     return (
-        <div class="navbarContainer">
-        <Link style={{ textDecoration: 'none' }} exact to="/">Home Page</Link>
-        <Link style={{ textDecoration: 'none' }} exact to="/App">Normal Game Mode</Link>
-        <Link style={{ textDecoration: 'none' }} exact to="/Appfree">Free Game Mode</Link>
-        <Link style={{ textDecoration: 'none' }} exact to="/Rules">Rules</Link>
+        <div>
+            <ul>   
+                <li><Link style={{ textDecoration: 'none' }} exact to="/">Home Page</Link></li>
+                <li><Link style={{ textDecoration: 'none' }} exact to="/App">Normal Game Mode</Link></li>
+                <li><Link style={{ textDecoration: 'none' }} exact to="/Appfree">Free Game Mode</Link></li>
+                <li><Link style={{ textDecoration: 'none' }} exact to="/Rules">Rules</Link></li>
+            </ul>
         </div>
     )
 }
