53e62b9baf6be087811feacc11a4864c51d660f5
Zidi Xia <xia.zid@northeastern.edu>
2021-11-18 18:09:03 -0800

    recreate

---
+++ src/Appfree.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import EnemyBoard from './Grid/EnemyBoard';
+import './App.css';
+import Controller from './Controller';
+
+function Appfree() {
+	return (
+		<div className="App">
+			<div className="title">
+				<h1>Battleship Free Game Mode</h1>
+				<Controller />
+			</div>
+			<div className="board-layout">
+				<div>
+					<EnemyBoard symbol={1} />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Appfree;
